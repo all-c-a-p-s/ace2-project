f30dd380457efd0af4392e263979f2345f5f5ea9
Seba <sebasonofriccardo@gmail.com>
2026-07-20 11:11:09 +0000

    change units

--- compare_outputs.py
+++ compare_outputs.py
@@ -18,11 +18,15 @@ lon = "longitude" if "longitude" in real.dims else "lon"
 weights = np.cos(np.deg2rad(real[lat]))
 
 rmse = np.sqrt(((artificial - real) ** 2).weighted(weights).mean((lat, lon)))
+rmse = rmse.squeeze(drop=True)
 
-rmse.plot()
+lead_days = rmse["time"] / (1e9 * 60 * 60 * 24)
+rmse_mm_day = rmse * 86400
+
+plt.plot(lead_days, rmse_mm_day)
 plt.title("Precipitation difference: artificial vs real forcing")
-plt.ylabel("Spatial RMSE of PRATEsfc")
-plt.xlabel("Forecast time")
+plt.xlabel("Forecast lead time (days)")
+plt.ylabel("Spatial RMSE (mm/day)")
 plt.tight_layout()
 plt.savefig("precipitation_difference.png", dpi=150)
 plt.show()
